aeefd3c6034260f51063cd8cc10beeb8a9571d6e
eugenegraves <elg22@njit.edu>
2024-02-28 02:43:23 -0500

    Finished Feature #2: View Top 5 Film Details

--- server.py
+++ server.py
@@ -15,12 +15,25 @@ app.config['MYSQL_DB'] = "sakila"
 #db = SQLAlchemy(app)
 mysql = MySQL(app)
 
-@app.route('/top_five', methods=['GET'])
-def displayTopFive():
+@app.route('/top_five_films', methods=['GET'])
+def displayTopFiveFilms():
     try:
         #data = request.json
         cur = mysql.connection.cursor()
-        cur.execute("SELECT actor.actor_id, actor.first_name, actor.last_name, COUNT(film_actor.film_id) AS film_count FROM actor JOIN film_actor ON actor.actor_id = film_actor.actor_id GROUP BY actor.actor_id ORDER BY film_count DESC LIMIT 5;")
+        cur.execute("SELECT f.film_id, f.title, COUNT(r.rental_id) AS num_rentals FROM film f JOIN inventory i ON f.film_id = i.film_id JOIN rental r ON i.inventory_id = r.inventory_id GROUP BY f.film_id, f.title ORDER BY num_rentals DESC LIMIT 5;")
+        data = cur.fetchall()
+        cur.close()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@app.route('/film_details', methods=['GET'])
+def displayFilmDetails():
+    try:
+        #data = request.json
+        title = request.args.get('title')
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT film_id, title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features, last_update FROM film WHERE title = %s;", (title,))
         data = cur.fetchall()
         cur.close()
         return jsonify(data)
