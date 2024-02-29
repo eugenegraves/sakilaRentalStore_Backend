55364efc0bbfc4139bc5d56170ea26b4fecf9c58
eugenegraves <elg22@njit.edu>
2024-02-28 20:12:34 -0500

    Feature #3 Almost Finished

--- server.py
+++ server.py
@@ -39,6 +39,51 @@ def displayFilmDetails():
         return jsonify(data)
     except Exception as e:
         return jsonify({'error': str(e)})
+    
+@app.route('/top_five_actors', methods=['GET'])
+def fetchTopFiveActors():
+    try:
+        #data = request.json
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            SELECT first_name, last_name, num_rentals
+            FROM (
+                SELECT a.actor_id, a.first_name, a.last_name, COUNT(r.rental_id) AS num_rentals
+                FROM actor a
+                JOIN film_actor fa ON a.actor_id = fa.actor_id
+                JOIN film f ON fa.film_id = f.film_id
+                JOIN inventory i ON f.film_id = i.film_id
+                JOIN rental r ON i.inventory_id = r.inventory_id
+                GROUP BY a.actor_id, a.first_name, a.last_name
+            ) AS rental_counts -- Add alias here
+            ORDER BY num_rentals DESC
+            LIMIT 5;
+        """)
+        data = cur.fetchall()
+        print(data)
+        cur.close()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@app.route('/actor_details', methods=['GET'])
+def displayActorDetails():
+    try:
+        #data = request.json
+        first_name = request.args.get('f_name')
+        last_name = request.args.get('l_name')
+        cur = mysql.connection.cursor()
+        cur.execute("""SELECT a.*, f.title AS film_title
+                    FROM actor a
+                    JOIN film_actor fa ON a.actor_id = fa.actor_id
+                    JOIN film f ON fa.film_id = f.film_id
+                    WHERE a.first_name = %s AND a.last_name = %s;
+                    """, (first_name, last_name,))
+        data = cur.fetchall()
+        cur.close()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 '''
 class User(db.Model):
